470270edb1affaf8825c58ab37d3be79f39d3943
baixiao <bzh2lf163@163.com>
2021-12-18 14:28:13 +0800

    init App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <div>
       <Button type='primary' onClick={handleClick}>Primary</Button>
+      <Button type='primary' onClick={handleClick}>Primary</Button>
     </div>
   )
 }
